75990ab26587925bc09979e76c626f276bac5899
personryan <mageryan1@gmail.com>
2023-05-13 21:47:12 +0800

    Update App.js
    edit the title and content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useEffect} from "react";
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavBar } from "./components/NavBar";
@@ -6,7 +6,18 @@ import { Container } from "@chakra-ui/react";
 import { Home } from "./pages/Home";
 import Project from "./pages/Project";
 import Work from "./pages/Work";
+
 export function App() {
+    const appTitle = 'personryan';
+    const appDescription = `personryan's personal website to showcase projects and work experiences`;
+
+    useEffect(() => {
+        document.title = appTitle;
+        const metaTag = document.querySelector('meta[name="description"]');
+        if (metaTag) {
+            metaTag.setAttribute('content', appDescription);
+        }
+    }, []);
     return (
         <>
             <ChakraProvider>
