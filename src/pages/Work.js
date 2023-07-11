08ad3fcf753a5e197d1a0d5b759ff8173ba61356
personryan <mageryan1@gmail.com>
2023-07-11 11:07:19 +0800

    Update Work.js

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -20,21 +20,25 @@ const workTitle = [
     'Finance Assistant',
     'Supply Chain Management Intern',
     'Tuition Teacher',
+    'Student Web Developer & Administrator',
 ];
 const workCompany = [
     'NTT Singapore Digital Business Solutions Pte Ltd',
     'Centre of Innovation @ Republic Polytechnic',
-    'Self-Sourced'
+    'Self-Sourced',
+    'Singapore Institute of Technology'
 ]
 const workDuration = [
     'Aug 2020 - Mar 2021',
     'Oct 2017 - Feb 2018',
     'Jan 2021 - Current',
+    'May 2023 - Current',
 ]
 const workDescription = [
     'Assisted in payment receiveable, invoices and monthly financial analysis.',
     'Created supply chain management solutions such as inventory planner & Robotic Process Automation(RPA) solutions',
     'Taught primary school students on mathematics and science',
+    'Develop webpage and update details'
 ]
 const workHighlight = [
     ['Created an Excel sheet to conduct monthly financial analysis, leading to a one-week reduction in the time for key stakeholders to receive the information',
@@ -45,7 +49,9 @@ const workHighlight = [
         'Showcased presentation skills by communicating the benefits and impact of RPA solutions clearly and concisely',
         'Created an Excel sheet that utilised excel functions to manage inventory for the client which resulted in reducing overbuying which saved costs by optimising inventory'],
     ['Demonstrated problem solving skills by employing a variety of teaching methods which resulted in an increase of the student grade from 50 to 70',
-        'Showcased Communication skills by clearly explaining complex questions which resulted in the student understanding and retaining the methods that are needed to solve the question']
+        'Showcased Communication skills by clearly explaining complex questions which resulted in the student understanding and retaining the methods that are needed to solve the question'],
+    ['Researched on markdown language and developed the SIT Cybersecurity Research Group webpage','Implemented HCI elements to enhance the visual appeal and user experience of the webpage', 
+        'Demonstrated strong communication skills by regularly presenting updates on the webpage to the supervisor during weekly reviews'],
 ]
 
 export function Work() {
